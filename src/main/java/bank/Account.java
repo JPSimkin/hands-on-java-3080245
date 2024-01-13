1fa6d26275a8150c865a8bf4a4e526030affae73
JPSimkin <147119375+JPSimkin@users.noreply.github.com>
2024-01-13 03:41:34 +0000

    modified:   src/main/java/bank/Account.java new file:   src/main/java/bank/Menu.java

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -12,7 +12,6 @@ public class Account {
 
   }
 
-
   public int getId() {
     return this.id;
   }
@@ -35,6 +34,24 @@ public class Account {
 
   public void setBalance(double balance) {
     this.balance = balance;
+  }
+
+  public void deposit(double amount) {
+    this.balance = this.balance + amount;
+
+  }
+
+  public void withdraw(double amount) {
+    if (amount < this.balance) {
+      this.balance = this.balance - amount;
+    } else if (amount == this.balance) {
+      this.balance = 0;
+    } else {
+      System.out.println("You have less than " + amount + " in your account.");
+      System.out.println("Your withdrawl cannot be made.");
+    }
+    
+
   }
 
 }
